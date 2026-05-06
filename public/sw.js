5336f9018f18ac6758e1c04290b09f53fb478ae5
Shane Logsdon <shane@shanelogsdon.com>
2026-05-06 17:36:13 -0400

    perf: bump SW cache to v12, load Google Fonts async
    Async font load via media=print trick eliminates render-blocking
    resource penalty. SW version bump forces cache refresh on next visit.

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open('shane-logsdon-io-static-v11').then((cache) => {
+    caches.open('shane-logsdon-io-static-v12').then((cache) => {
       return cache.addAll(["/"]).catch(() => {});
     })
   );
@@ -12,7 +12,7 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((keys) =>
       Promise.all(
         keys
-          .filter((key) => key !== 'shane-logsdon-io-static-v11')
+          .filter((key) => key !== 'shane-logsdon-io-static-v12')
           .map((key) => caches.delete(key))
       )
     ).then(() => self.clients.claim())
